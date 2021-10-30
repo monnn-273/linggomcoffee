859ec549ce0a8a5b02f2146186829421d74545c4
Monika Panjaitan <monikamedan0102@gmail.com>
2021-10-30 12:52:54 +0700

    sinkronisasi dengan template danny

--- resources/views/administrator/dashboard.blade.php
+++ resources/views/administrator/dashboard.blade.php
@@ -1,1 +1,19 @@
-<h1>INI ADALAH DASHBOARD ADMINISTRATOR</h1>
+@extends('administrator.headerfooter')
+
+@section ('content')
+
+<table>
+    <tr>
+        <td>HEADER</td>
+    </tr>
+    <tr>
+        <td>item1</td>
+    </tr>
+    <tr>
+        <td>item2</td>
+    </tr>
+
+</table>
+
+@endsection ('content')
+
---
+++ resources/views/administrator/headerfooter.blade.php
@@ -0,0 +1,189 @@
+<?php
+ use Illuminate\Support\Facades\Auth;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <title>Linggom Coffee - Admin</title>
+      <!-- Eatery Cafe Template http://www.templatemo.com/tm-515-eatery -->
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="author" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+     <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+     <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
+     <link rel="stylesheet" href="{{asset('css/animate.css')}}">
+     <link rel="stylesheet" href="{{asset('css/owl.carousel.css')}}">
+     <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
+     <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
+
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="{{asset('css/templatemo-style.css')}}">
+</head>
+
+<body>
+
+    <!-- PRE LOADER -->
+    <section class="preloader">
+        <div class="spinner">
+            <span class="spinner-rotate"></span>  
+        </div>
+    </section>
+
+
+    <!-- INTRODUCTION : HERO SECTION -->
+    <section class="navbar custom-navbar navbar-fixed-top" role="navigation">
+          <div class="container">
+
+               <div class="navbar-header">
+                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                    </button>
+
+                    <!-- LOGO TEXT HERE -->
+                    <a href="index.html" class="navbar-brand">Linggom <span>.</span> Coffee</a>
+               </div>
+
+               <!-- MENU LINKS -->
+               <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-nav-first">
+                         <li><a href="#home" class="smoothScroll">Profile</a></li>
+                         <li><a href="#about" class="smoothScroll">Order List</a></li>
+                         <li><a href="#contact" class="smoothScroll">Produk</a></li>
+                    </ul>
+
+                    @auth
+                        <ul class="nav navbar-nav navbar-right">
+                              <a id="navbarDropdown" class="section-btn" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                   {{ Auth::user()->name }}
+                              </a>
+                              <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                  document.getElementById('logout-form').submit();">
+                                   {{ __('Logout') }}
+                              </a>
+
+                              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                   @csrf
+                              </form>
+
+                        </ul>
+                    @else
+                        <ul class="nav navbar-nav navbar-right">
+                            <a href="{{__('/register')}}" class="section-btn">Daftar</a>
+                            <a href="{{__('/login')}}" class="section-btn">Masuk</a>
+                        </ul>
+                    @endauth
+            </div>
+          </div>
+     </section>
+
+    <!-- CONTENT -->
+    @yield('content')
+
+     <!-- FOOTER -->
+     <footer id="footer" data-stellar-background-ratio="0.5">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-3 col-sm-8">
+                         <div class="footer-info">
+                              <div class="section-title">
+                                   <h2 class="wow fadeInUp" data-wow-delay="0.2s">Find us</h2>
+                              </div>
+                              <address class="wow fadeInUp" data-wow-delay="0.4s">
+                                   <p>123 nulla a cursus rhoncus,<br> augue sem viverra 10870<br>id ultricies sapien</p>
+                              </address>
+                         </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-8">
+                         <div class="footer-info footer-open-hour">
+                              <div class="section-title">
+                                   <h2 class="wow fadeInUp" data-wow-delay="0.2s">Open Hours</h2>
+                              </div>
+                              <div class="wow fadeInUp" data-wow-delay="0.4s">
+                                   <p>Monday: Closed</p>
+                                   <div>
+                                        <strong>Tuesday to Friday</strong>
+                                        <p>7:00 AM - 9:00 PM</p>
+                                   </div>
+                                   <div>
+                                        <strong>Saturday - Sunday</strong>
+                                        <p>11:00 AM - 10:00 PM</p>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+
+                    <div class="col-md-2 col-sm-4">
+                         <ul class="wow fadeInUp social-icon" data-wow-delay="0.4s">
+                              <li><a href="#" class="fa fa-facebook-square" attr="facebook icon"></a></li>
+                              <li><a href="#" class="fa fa-twitter"></a></li>
+                              <li><a href="#" class="fa fa-instagram"></a></li>
+                              <li><a href="#" class="fa fa-google"></a></li>
+                         </ul>
+
+                         <div class="wow fadeInUp copyright-text" data-wow-delay="0.8s"> 
+                              <p><br>Copyright &copy; 2021 <br>Linggom Coffee
+                              
+                              <br><br>Design: <a rel="nofollow" href="http://templatemo.com" target="_parent">TemplateMo</a></p>
+                         </div>
+                    </div>
+                    
+               </div>
+          </div>
+     </footer>
+
+
+     <!-- SCRIPTS -->
+     <script src="{{asset('js/jquery.js')}}"></script>
+     <script src="{{asset('js/bootstrap.min.js')}}"></script>
+     <script src="{{asset('js/jquery.stellar.min.js')}}"></script>
+     <script src="{{asset('js/wow.min.js')}}"></script>
+     <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+     <script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+     <script src="{{asset('js/smoothscroll.js')}}"></script>
+     <script src="{{asset('js/custom.js')}}"></script>
+
+</body>
+</html>
+
+<!-- Authentication Links -->
+<!-- @guest
+    @if (Route::has('login'))
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+        </li>
+    @endif
+
+    @if (Route::has('register'))
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+        </li>
+    @endif
+@else
+    <li class="nav-item dropdown">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            {{ Auth::user()->name }}
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        </div>
+    </li>
+@endguest -->
